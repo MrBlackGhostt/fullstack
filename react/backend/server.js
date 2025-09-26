e667dd75c6e5fcdbc3bb597c718c865b5960ef7a
MrBlackGhostt <hemantkumar4213@icloud.com>
2025-09-26 12:07:03 +0530

    add the delete put api

--- react/backend/server.js
+++ react/backend/server.js
@@ -45,27 +45,58 @@ app.post("/data", async (req, res) => {
 });
 
 app.get("/data", async (req, res) => {
-  const id = req.query.id;
-  console.log("🚀 -----------🚀");
-  console.log("🚀 ~ id:", id);
-  console.log("🚀 -----------🚀");
+  const { id } = req.query;
+  const { accept } = req.headers || req.query;
+
   const { rows } = await client.query(`SELECT * FROM json_store WHERE id=$1`, [
     id,
   ]);
   if (rows.length > 0) {
-    res.type("application/json");
+    res.type("html");
     res
       .status(200)
       .send({ "Content-type": "application-type/json", message: rows[0].data });
+    return;
   }
   res.status(404).send({ "Content-type": "text", message: "not found" });
 });
 
-app.put("/data:id", (req, res) => {
-  const { id } = req.params("id");
+app.put("/data/:id", async (req, res) => {
+  // const { id } = req.params;
+  const { id } = req.query;
   console.log("🚀 -----------🚀");
   console.log("🚀 ~ id:", id);
   console.log("🚀 -----------🚀");
+  const body = req.body;
+  const query = "UPDATE json_store SET data = $1 WHERE id = $2 RETURNING *";
+  try {
+    const { rows } = await client.query(query, [body, id]);
+    console.log("🚀 ---------------🚀");
+    console.log("🚀 ~ rows:", rows);
+    console.log("🚀 ---------------🚀");
+    console.log(rows[0].data);
+    res.status(200).send({ message: "updated" });
+  } catch (error) {
+    console.error("Something went wrong while updating", error);
+    res.status(500).send({ message: "not updated" });
+  }
+});
+
+app.delete("/data/:id", async (req, res) => {
+  const { id } = req.params;
+  const query = "DELETE FROM json_store WHERE id=$1 RETURNING *";
+  try {
+    const { rows } = await client.query(query, [id]);
+    console.log("🚀 ---------------🚀");
+    console.log("🚀 ~ rows:", rows[0].data);
+    console.log("🚀 ---------------🚀");
+    res
+      .status(200)
+      .send({ message: "delete successfully", data: rows[0].data });
+  } catch (error) {
+    console.error("ERROR", error);
+    res.status(500).send({ message: "delete unsuccessfully" });
+  }
 });
 
 app.listen(3000, () => {
